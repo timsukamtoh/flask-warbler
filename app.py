3abba8bfa1f9b285ad9260714b74ee7013271bef
Timothy Sukamtoh <timtoh168@gmail.com>
2023-07-25 14:48:12 -0700

    disabled csrf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import functools
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request, flash, redirect, session, g
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtectForm
@@ -19,6 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['WTF_CSRF_ENABLED'] = False
 # toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
